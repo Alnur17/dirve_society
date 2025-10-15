4a31b7a45644be5faaf28ea35c3e1780d7b2a0fd
Md Aminul Islam <aminulappdev@gmail.com>
2025-10-15 16:59:12 +0600

    update

--- lib/urls.dart
+++ lib/urls.dart
@@ -1,7 +1,7 @@
 class Urls {
   // static const String _baseUrl = 'http://10.10.10.16:5006/api/v1';
-  static const String _baseUrl = 'http://206.162.244.133:5106/api/v1';
-  static const String socketUrl = 'http://206.162.244.133:4106/';
+  static const String _baseUrl = 'http://74.118.168.203:5006/api/v1';
+  static const String socketUrl = 'http://74.118.168.203:4006/';
   // static const String socketUrl = 'http://192.168.10.144:4001/';
   static const String signUpUrl = '$_baseUrl/profiles/register';
   static const String otpVerifyUrl = '$_baseUrl/otp/verify-otp';
